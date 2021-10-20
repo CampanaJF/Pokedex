874d8ca66bc01f7118e33c8fa4c1060db4a37ac9
CampanaJF <jucampana@alumno.unlam.edu.ar>
2021-10-20 20:27:31 -0300

    Arreglos post push a la bd

--- config/bd_pokedex.sql
+++ config/bd_pokedex.sql
@@ -4,7 +4,7 @@ use pokedex;
 
 create table pokemon(
 id int auto_increment primary key,
-numero int,
+numero int unique,
 nombre varchar(20),
 tipo1 varchar(20),
 tipo2 varchar(20),
@@ -21,13 +21,21 @@ esAdmin int
 );
 
 insert into usuario (nombre,contraseña,esAdmin)
-			 values("admin","admin",1);
+			 values("admin",md5("admin"),1);
              
 insert into pokemon (numero,nombre,tipo1,tipo2,descripcion,img)
-              values(094,"Gengar","Tipo_veneno.jepg","Tipo_fantasma.jpeg",
+              values(094,"Gengar","Tipo_veneno","Tipo_fantasma",
               "Gengar es un Pokémon de tipo fantasma/veneno introducido en la primera generación. Es la evolución de Haunter y,
               a partir de la sexta generación, puede megaevolucionar en Mega-Gengar. ","Gengar.jpeg");
 
+insert into pokemon (numero,nombre,tipo1,descripcion,img)
+              values(143,"Snorlax","Tipo_normal",
+              "Snorlax es un Pokémon de tipo normal introducido en la primera generación.
+              A partir de la cuarta generación tiene una preevolución llamada Munchlax. ","Snorlax.jpeg");
+              
+insert into pokemon (numero,nombre,tipo1,tipo2,descripcion,img)
+              values(093,"Haunter","Tipo_veneno","Tipo_fantasma",
+              "Haunter es un Pokémon de tipo fantasma/veneno introducido en la primera generación. Es la evolución de Gastly. ","Haunter.jpeg");
 
 insert into pokemon (numero,nombre,tipo1,tipo2,descripcion)
 values(006,"Charizard","Tipo_fuego","Tipo_volador",
